5e93e0bf66b05604712cd5de31d3f34fb253132f
Mr Matsela <malefane@vut.ac.za>
2025-05-06 12:53:42 +0200

    First commit

---
+++ src/main/java/com/vut/SimpleAddition.java
@@ -0,0 +1,11 @@
+package com.vut;
+
+/**
+ * Simple Addition method
+ *
+ */
+public class SimpleAddition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/vut/SimpleAdditionTest.java
@@ -0,0 +1,18 @@
+package com.vut;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple additon.
+ */
+public class SimpleAdditionTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
